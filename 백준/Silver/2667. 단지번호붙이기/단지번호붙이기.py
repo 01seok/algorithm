8c386cd66b31a0b052fdcb355604400368c7519d
SEOKJAE <lsj6924@naver.com>
2025-07-12 01:33:08 +0900

    [Silver I] Title: 단지번호붙이기, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,38 +1,37 @@
-N = int(input())
-graph = [list(map(int, input())) for _ in range(N)]
+N =int(input())
+apt = [list(map(int, input())) for _ in range(N)]
 visited = [[False] * N for _ in range(N)]
-result = []
+ans = []
 
-# 상하좌우
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
+dr = (-1, 1, 0, 0)
+dc = (0, 0, -1, 1)
 
 def dfs(r, c):
-    stack = [(r, c)]
-    count = 0
+    stack = [(r,c)]
+    cnt = 0
 
     while stack:
-        cr, cc = stack.pop()
-        if visited[cr][cc]:
+        cur_r, cur_c = stack.pop()
+
+        if visited[cur_r][cur_c]:
             continue
-        visited[cr][cc] = True
-        count += 1
+
+        visited[cur_r][cur_c] = True
+        cnt += 1
 
         for d in range(4):
-            nr = cr + dr[d]
-            nc = cc + dc[d]
+            nr, nc = cur_r + dr[d], cur_c + dc[d]
             if 0 <= nr < N and 0 <= nc < N:
-                if not visited[nr][nc] and graph[nr][nc] == 1:
+                if apt[nr][nc] == 1 and not visited[nr][nc]:
                     stack.append((nr, nc))
-
-    return count
+    return cnt
 
 for r in range(N):
     for c in range(N):
-        if graph[r][c] == 1 and not visited[r][c]:
-            result.append(dfs(r, c))
+        if apt[r][c] == 1 and not visited[r][c]:
+            ans.append(dfs(r,c))
 
-result.sort()
-print(len(result))
-for x in result:
-    print(x)
+ans.sort()
+print(len(ans))
+for i in ans:
+    print(i)
