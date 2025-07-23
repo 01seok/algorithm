72fcb3c4f0a8b57c54556174fdd5af47121456dc
SEOKJAE <lsj6924@naver.com>
2025-07-23 23:53:24 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A =  Integer.parseInt(st.nextToken());
+        int B =  Integer.parseInt(st.nextToken());
+        int V =  Integer.parseInt(st.nextToken());
+
+        int day;
+
+        if (V <= A) {
+            day = 1;
+        } else {
+            int targetRemainHeight = V - A;
+            int dailyUp = A - B;
+
+            day = (int) Math.ceil((double) targetRemainHeight / dailyUp);
+            day += 1;
+        }
+        System.out.println(day);
+    }
+}
+
