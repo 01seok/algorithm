d295605655a23a095f8f510b949fa4f6343cdd0c
SEOKJAE <lsj6924@naver.com>
2025-08-06 19:52:47 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] submitted = new boolean[31];
+        for (int i = 0; i < 28; i++) {
+            int num = Integer.parseInt(br.readLine());
+            submitted[num] = true;
+        }
+        for (int i = 1; i <= 30; i++) {
+            if (!submitted[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
