369b3f4fb4d116d093d36b752a30c25a7e8004dd
SEOKJAE <lsj6924@naver.com>
2025-08-06 19:51:51 +0900

    [Bronze II] Title: 다이얼, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,9 @@
+word = input()
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+time = 0
+for ch in word:
+    for i in range(len(dial)):
+        if ch in dial[i]:
+            time += i + 3
+            break
+print(time)
