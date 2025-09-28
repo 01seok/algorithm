9a23666d2b28506f3af9d11ec00d62d8e3095747
SEOKJAE <lsj6924@naver.com>
2025-09-28 18:09:50 +0900

    [Silver V] Title: 단어 정렬, Time: 300 ms, Memory: 26328 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,18 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        HashSet<String> uniqueWordList = new HashSet<>();
-        for (int i = 0; i < N; i++) {
-            uniqueWordList.add(br.readLine());
-        }
-        ArrayList<String> wordList = new ArrayList<>(uniqueWordList);
 
-        Collections.sort(wordList, new Comparator<String>() {
+        String[] arr = new String[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
                 if (s1.length() == s2.length()) {
@@ -24,10 +26,12 @@ public class Main {
         });
 
         StringBuilder sb = new StringBuilder();
-        for (String word : wordList) {
-            sb.append(word).append("\n");
+        sb.append(arr[0]).append('\n');
+        for (int i = 1; i < N; i++) {
+            if (!arr[i].equals(arr[i - 1])) {
+                sb.append(arr[i]).append('\n');
+            }
         }
-        System.out.println(sb.toString());
+        System.out.print(sb.toString());
     }
 }
-
