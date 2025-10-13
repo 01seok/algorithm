5aff4cae505700ef3ddcae09efb10be72b597cb0
SEOKJAE <lsj6924@naver.com>
2025-10-13 18:01:15 +0900

    [Bronze I] Title: 약수, Time: 176 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] divisors = new int[n];
+        for (int i = 0; i < n; i++) divisors[i] = sc.nextInt();
+        Arrays.sort(divisors);
+        System.out.println(divisors[0] * divisors[n - 1]);
+    }
+}
