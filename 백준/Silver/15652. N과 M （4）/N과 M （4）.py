7ef6ef81f51b69f803d6dc63bd92e0749f7fcfc0
SEOKJAE <lsj6924@naver.com>
2026-03-19 00:42:39 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 112180 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+seq = []
+
+def backtracking(start):
+    if len(seq) == M:
+        print(*seq)
+        return
+
+    for i in range(start, N+1):
+        seq.append(i)
+        backtracking(i)
+        seq.pop()
+
+backtracking(1)
